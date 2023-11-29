1e7f594f1c50bf86af66499bc1ad892f70e0382b
Luu Thuy Minh Nhat <minhnhat240820032019@gmail.com>
2023-11-29 14:16:06 +0700

    add Bai1

---
+++ Bai1/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Bai1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbMaKhoa = new System.Windows.Forms.Label();
+            this.lbTenKhoa = new System.Windows.Forms.Label();
+            this.txtMaKhoa = new System.Windows.Forms.TextBox();
+            this.txtTenKhoa = new System.Windows.Forms.TextBox();
+            this.dgvKhoa = new System.Windows.Forms.DataGridView();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.btnTimKiem = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvKhoa)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbMaKhoa
+            // 
+            this.lbMaKhoa.AutoSize = true;
+            this.lbMaKhoa.Location = new System.Drawing.Point(94, 33);
+            this.lbMaKhoa.Name = "lbMaKhoa";
+            this.lbMaKhoa.Size = new System.Drawing.Size(50, 13);
+            this.lbMaKhoa.TabIndex = 0;
+            this.lbMaKhoa.Text = "Ma Khoa";
+            // 
+            // lbTenKhoa
+            // 
+            this.lbTenKhoa.AutoSize = true;
+            this.lbTenKhoa.Location = new System.Drawing.Point(94, 69);
+            this.lbTenKhoa.Name = "lbTenKhoa";
+            this.lbTenKhoa.Size = new System.Drawing.Size(54, 13);
+            this.lbTenKhoa.TabIndex = 1;
+            this.lbTenKhoa.Text = "Ten Khoa";
+            // 
+            // txtMaKhoa
+            // 
+            this.txtMaKhoa.Location = new System.Drawing.Point(160, 33);
+            this.txtMaKhoa.Name = "txtMaKhoa";
+            this.txtMaKhoa.Size = new System.Drawing.Size(131, 20);
+            this.txtMaKhoa.TabIndex = 2;
+            // 
+            // txtTenKhoa
+            // 
+            this.txtTenKhoa.Location = new System.Drawing.Point(160, 69);
+            this.txtTenKhoa.Name = "txtTenKhoa";
+            this.txtTenKhoa.Size = new System.Drawing.Size(131, 20);
+            this.txtTenKhoa.TabIndex = 3;
+            // 
+            // dgvKhoa
+            // 
+            this.dgvKhoa.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvKhoa.Location = new System.Drawing.Point(97, 122);
+            this.dgvKhoa.Name = "dgvKhoa";
+            this.dgvKhoa.Size = new System.Drawing.Size(414, 185);
+            this.dgvKhoa.TabIndex = 4;
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(332, 29);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(75, 23);
+            this.btnThem.TabIndex = 5;
+            this.btnThem.Text = "Them";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(436, 31);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(75, 23);
+            this.btnXoa.TabIndex = 6;
+            this.btnXoa.Text = "Xoa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnSua
+            // 
+            this.btnSua.Location = new System.Drawing.Point(332, 69);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(75, 23);
+            this.btnSua.TabIndex = 7;
+            this.btnSua.Text = "Sua";
+            this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // btnTimKiem
+            // 
+            this.btnTimKiem.Location = new System.Drawing.Point(436, 69);
+            this.btnTimKiem.Name = "btnTimKiem";
+            this.btnTimKiem.Size = new System.Drawing.Size(75, 23);
+            this.btnTimKiem.TabIndex = 8;
+            this.btnTimKiem.Text = "Tim Kiem";
+            this.btnTimKiem.UseVisualStyleBackColor = true;
+            this.btnTimKiem.Click += new System.EventHandler(this.btnTimKiem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(611, 370);
+            this.Controls.Add(this.btnTimKiem);
+            this.Controls.Add(this.btnSua);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.dgvKhoa);
+            this.Controls.Add(this.txtTenKhoa);
+            this.Controls.Add(this.txtMaKhoa);
+            this.Controls.Add(this.lbTenKhoa);
+            this.Controls.Add(this.lbMaKhoa);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvKhoa)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbMaKhoa;
+        private System.Windows.Forms.Label lbTenKhoa;
+        private System.Windows.Forms.TextBox txtMaKhoa;
+        private System.Windows.Forms.TextBox txtTenKhoa;
+        private System.Windows.Forms.DataGridView dgvKhoa;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button btnTimKiem;
+    }
+}
+
---
+++ Bai1/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace Bai1
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn;
+        SqlDataAdapter da_khoa;
+        DataSet ds_khoa;
+        DataColumn[] key = new DataColumn[1];
+        SqlCommand cmd = new SqlCommand();
+        string strsql;
+        public Form1()
+        {
+            InitializeComponent();
+            conn = new SqlConnection("Data Source=A209PC02;Initial Catalog=QL_Khoa;Integrated Security=True");
+            string strSelect = "select * from Khoa";
+            da_khoa = new SqlDataAdapter(strSelect, conn);
+            ds_khoa = new DataSet();
+            da_khoa.Fill(ds_khoa, "Khoa");
+            key[0] = ds_khoa.Tables["Khoa"].Columns[0];
+            ds_khoa.Tables["Khoa"].PrimaryKey = key;
+        }
+        void Databingding(DataTable pDT)
+        {
+            txtMaKhoa.DataBindings.Clear();
+            txtTenKhoa.DataBindings.Clear();
+            //lien ket du lieu trong text box voi truong du lieu trong database
+            txtMaKhoa.DataBindings.Add("Text", pDT, "Ma Khoa");
+            txtTenKhoa.DataBindings.Add("Text", pDT, "Ten Khoa");
+        }
+        void loadGrid()
+        {
+            dgvKhoa.DataSource = ds_khoa.Tables[0];
+            Databingding(ds_khoa.Tables[0]);
+        }
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            //tao 1 dong du lieu moi
+            DataRow newrow = ds_khoa.Tables[0].NewRow();
+            newrow["Ma Khoa"] = txtMaKhoa.Text;
+            newrow["Ten Khoa"] = txtTenKhoa.Text;
+            //them dong du lieu
+            ds_khoa.Tables[0].Rows.Add(newrow);
+            //cap nhap trong database
+            SqlCommandBuilder cB = new SqlCommandBuilder(da_khoa);
+            //cap nhat dataset
+            da_khoa.Update(ds_khoa, "Khoa");
+            txtMaKhoa.Clear();
+            txtTenKhoa.Clear();
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            DataRow dr = ds_khoa.Tables[0].Rows.Find(txtMaKhoa.Text);
+            if(dr!=null)
+            {
+                dr.Delete();
+            }
+            //cap nhap trong database
+            SqlCommandBuilder cB = new SqlCommandBuilder(da_khoa);
+            //cap nhat dataset
+            da_khoa.Update(ds_khoa, "Khoa");
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            DataRow dr = ds_khoa.Tables[0].Rows.Find(txtMaKhoa.Text);
+            if(dr!=null)
+            {
+                dr["Ten Khoa"] = txtTenKhoa.Text;
+            }
+            //cap nhap trong database
+            SqlCommandBuilder cB = new SqlCommandBuilder(da_khoa);
+            //cap nhat dataset
+            da_khoa.Update(ds_khoa, "Khoa");
+        }
+
+        private void btnTimKiem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            loadGrid();
+        }
+    }
+}
